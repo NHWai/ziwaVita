8de2377e4140dc0833c7e2a3af55894fea45f197
NHWai <nayhtetwinee@gmail.com>
2022-02-08 09:45:03 +0630

    before using swiper

--- script.js
+++ script.js
@@ -32,8 +32,9 @@ const articlesContainer = document.querySelector(".articles--container");
 const leftArrow = document.querySelector(".articles .fa-chevron-left");
 const rightArrow = document.querySelector(".articles .fa-chevron-right");
 
-let currSlide = 0;
 articles.forEach((a, i) => (a.style.transform = `translateX(${100 * i}%)`));
+
+let currSlide = 0;
 
 const nextSlide = () => {
   currSlide++;
@@ -63,8 +64,6 @@ window.addEventListener("load", () => {
   console.log("hayy");
 });
 
-// clearInterval(timeout);
-
 rightArrow.addEventListener("click", function () {
   nextSlide();
   clearInterval(timeout);
@@ -73,3 +72,87 @@ leftArrow.addEventListener("click", function () {
   prevSlide();
   clearInterval(timeout);
 });
+
+/* Cards Animation */
+
+const bluecards = document.querySelectorAll(".bluecard");
+const redcards = document.querySelectorAll(".redcard");
+const valueSection = document.querySelector(".values .secondaryHeader");
+const missionSection = document.querySelector(".missions .secondaryHeader");
+
+const add = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting)
+    bluecards.forEach((card) => card.classList.add("start"));
+};
+
+const valueSectionObserver = new IntersectionObserver(add, {
+  root: null,
+  threshold: 1,
+  // rootMargin: "-200px",
+});
+
+valueSectionObserver.observe(valueSection);
+
+const msadd = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting)
+    redcards.forEach((card) => card.classList.add("start"));
+};
+
+const missionSectionObserver = new IntersectionObserver(msadd, {
+  root: null,
+  threshold: 1,
+  // rootMargin: "-200px",
+});
+
+missionSectionObserver.observe(missionSection);
+
+/*------------------------Latest Articles----------------------------*/
+
+const container = [
+  {
+    img: "img/latestarticlesImg1",
+    header: "အထွေထွေကျန်းမာရေးဆောင်းပါးများ",
+    date: "15 November 2021",
+    description: "သွေးတိုး နဲ့ ဆီးချို ဘယ်လိုတွေဆက်စပ်နေလဲ?",
+  },
+  {
+    img: "img/latestarticlesImg2",
+    header: "အထွေထွေကျန်းမာရေးဆောင်းပါးများ",
+    date: "01 November 2021",
+    description: "ဘိုးဘိုးဘွားဘွားတို့ မတော်တဆထိခိုက်ချော်လဲခြင်းမရှိစေဖို့",
+  },
+  {
+    img: "img/latestarticlesImg3",
+    header: "မျိုးဆက်ပွားကျန်းမာရေးဆိုင်ရာ ဆောင်းပါးများ",
+    date: "05 December 2021",
+    description: "DMPA-SC ကိုဘာကြောင့်အသုံးပြုသင့်သလဲ",
+  },
+];
+
+const coreContainer = document.querySelector(".core-container");
+
+const artFunc = function () {
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("latestarticles--cardcontainer");
+
+  container.forEach((el) => {
+    const html = `
+      <div class="latestarticles--card">
+      <img src="${el.img}.png" />
+      <p class="descriptionSM">${el.header}</p>
+      <p class="date">${el.date}</p>
+      <p class="description">${el.description}</p>
+      <button class="btnSm">Read More</button>
+      </div>
+  `;
+    cardContainer.insertAdjacentHTML("beforeend", html);
+  });
+
+  coreContainer.insertAdjacentElement("beforeend", cardContainer);
+};
+
+artFunc();
